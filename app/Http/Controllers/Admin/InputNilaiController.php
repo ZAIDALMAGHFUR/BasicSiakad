5116ce629d9370fb60ede950c1b2195960c4b956
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2023-03-13 18:45:22 +0700

    final change query builder to elequen

--- app/Http/Controllers/Admin/InputNilaiController.php
+++ app/Http/Controllers/Admin/InputNilaiController.php
@@ -13,60 +13,50 @@ class InputNilaiController extends Controller
 {
     public function index()
     {
-        $data_tahun_akademik = DB::select("
-                                    SELECT id,semester,CONCAT(tahun_akademik,'/') AS th_akademik
-                                    FROM tahun_akademik
-                                ");
-
+        $data_tahun_akademik = TahunAkademik::select('id', 'semester', DB::raw("CONCAT(tahun_akademik,'/') AS th_akademik"))->get();
         return view('admin.input_nilai.index', compact('data_tahun_akademik'));
     }
+    
 
     public function all(Request $request)
-    {
-        $request->validate([
-            'tahun_akademik_id' => 'required',
-            'kode_mata_kuliah' => 'required'
+{
+    $request->validate([
+        'tahun_akademik_id' => 'required',
+        'kode_mata_kuliah' => 'required'
+    ]);
+
+    $mata_kuliah = MataKuliah::where('kode_mata_kuliah', $request->kode_mata_kuliah)->firstOrFail();
+
+    $list_nilai = DB::table('krs')
+                        ->join('mahasiswa', 'mahasiswa.nim', '=', 'krs.nim')
+                        ->join('mata_kuliah', 'mata_kuliah.id', '=', 'krs.mata_kuliah_id')
+                        ->where('krs.tahun_akademik_id', $request->tahun_akademik_id)
+                        ->where('krs.mata_kuliah_id', $mata_kuliah->id)
+                        ->select('krs.id', 'krs.nim', 'mahasiswa.nama_lengkap', 'krs.nilai', 'mata_kuliah.nama_mata_kuliah')
+                        ->get();
+
+    $th_akademik = TahunAkademik::findOrFail($request->tahun_akademik_id)->tahun_akademik;
+    $semester = TahunAkademik::findOrFail($request->tahun_akademik_id)->semester;
+
+    if($list_nilai->isEmpty()) {
+        return redirect()->back()->with([
+            'message' => "mahasiswa tidak terdaftar di tahun ajaran $th_akademik ($semester)",
+            'alert-type' => 'info'
         ]);
- 
-        $mata_kuliah = MataKuliah::where('kode_mata_kuliah', $request->kode_mata_kuliah)->first();
-        if(is_null($mata_kuliah)) {
-             return redirect()->back()->with([
-                 'message' => 'mata kuliah tidak terdaftar!',
-                 'alert-type' => 'info'
-             ]);
-        }
-
-        $list_nilai = DB::select("SELECT 
-                                k.id,k.nim,m.nama_lengkap,k.nilai,mk.nama_mata_kuliah
-                            FROM krs AS k
-                            JOIN mahasiswa AS m 
-                            ON m.nim = k.nim
-                            JOIN mata_kuliah AS mk 
-                            ON k.mata_kuliah_id = mk.id
-                            WHERE k.tahun_akademik_id = $request->tahun_akademik_id
-                            AND k.mata_kuliah_id = $mata_kuliah->id
-                            ");
-            $th_akademik =  TahunAkademik::where('id', $request->tahun_akademik_id)->first()->tahun_akademik;
-            $sms = TahunAkademik::where('id', $request->tahun_akademik_id)->first()->semester;
-
-            if(count($list_nilai) == 0) {
-                return redirect()->back()->with([
-                    'message' => "mahasiswa tidak terdaftar di tahun ajaran $th_akademik ($sms)",
-                    'alert-type' => 'info'
-                ]);
-           }
-            $data = [
-                'list_nilai' => $list_nilai,
-                'kode_mata_kuliah' => $request->kode_mata_kuliah,
-                'nama_mata_kuliah' => $mata_kuliah->nama_mata_kuliah,
-                'tahun_akademik' => $th_akademik,
-                'semester' => $sms,
-                'sks' => $mata_kuliah->sks
-            ];
-
-            // return view('input_nilai.show', compact('data'));
-            return view('admin.input_nilai.show', compact('data'));
     }
+
+    $data = [
+        'list_nilai' => $list_nilai,
+        'kode_mata_kuliah' => $request->kode_mata_kuliah,
+        'nama_mata_kuliah' => $mata_kuliah->nama_mata_kuliah,
+        'tahun_akademik' => $th_akademik,
+        'semester' => $semester,
+        'sks' => $mata_kuliah->sks
+    ];
+
+    return view('admin.input_nilai.show', compact('data'));
+}
+
 
     public function store(Request $request)
     {
@@ -80,7 +70,6 @@ class InputNilaiController extends Controller
 
         $krs = Krs::with('mata_kuliah','tahun_akademik')->find($ids[0]);
         
-        // return view('input_nilai.daftar_nilai', compact('krs','ids'));
         return view('admin.input_nilai.daftar_nilai', compact('krs','ids'));
     }
 }
--- app/Http/Controllers/Admin/KrsController.php
+++ app/Http/Controllers/Admin/KrsController.php
@@ -13,67 +13,67 @@ use App\Http\Requests\Admin\KrsRequest;
 
 class KrsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        $data_tahun_akademik = DB::select("
-                            SELECT id,semester,CONCAT(tahun_akademik,'/') AS th_akademik
-                            FROM tahun_akademik
-                        ");
+        $data_tahun_akademik = TahunAkademik::select('id', 'semester', DB::raw("CONCAT(tahun_akademik,'/') AS th_akademik"))->get();
         return view('admin.krs.index', compact('data_tahun_akademik'));
     }
+    
 
     public function find(Request $request)
-    {
-       $request->validate([
-           'nim' => 'required',
-           'tahun_akademik_id' => 'required'
-       ]);
+{
+    $request->validate([
+        'nim' => 'required',
+        'tahun_akademik_id' => 'required'
+    ]);
 
-       $mhs = Mahasiswa::where('nim', $request->nim)->first();
-       if(is_null($mhs)) {
-            return redirect()->back()->with([
-                'message' => 'mahasiswa belum terdaftar !',
-                'alert-type' => 'info'
-            ]);
-       }
-
-       $select_krs = DB::select("select k.id, m.nama_mata_kuliah,m.kode_mata_kuliah,m.sks
-                            from krs as k
-                            join mata_kuliah as m on m.id = k.mata_kuliah_id
-                            where k.nim = $request->nim and k.tahun_akademik_id = $request->tahun_akademik_id
-                        ");
-       $data_krs = [
-           'nim' => $request->nim,
-           'tahun_akademik_id' => $request->tahun_akademik_id,
-           'nama_lengkap' => $mhs->nama_lengkap,
-           'tahun_akademik' => TahunAkademik::where('id', $request->tahun_akademik_id)->first()->tahun_akademik,
-           'semester' => TahunAkademik::where('id', $request->tahun_akademik_id)->first()->semester,
-           'prody' => $mhs->program_study->nama_prody,
-           'select_krs' => $select_krs
-       ];
-
-        return view('admin.krs.show', compact('data_krs'));
-        // return view('admin.krs.show', compact('data_krs'));
+    $mhs = Mahasiswa::where('nim', $request->nim)->first();
+    if(is_null($mhs)) {
+        return redirect()->back()->with([
+            'message' => 'mahasiswa belum terdaftar !',
+            'alert-type' => 'info'
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    $select_krs = Krs::where('nim', $request->nim)
+                ->where('tahun_akademik_id', $request->tahun_akademik_id)
+                ->join('mata_kuliah', 'krs.mata_kuliah_id', '=', 'mata_kuliah.id')
+                ->select('krs.id', 'mata_kuliah.nama_mata_kuliah', 'mata_kuliah.kode_mata_kuliah', 'mata_kuliah.sks')
+                ->get();
+
+    if(count($select_krs) == 0) {
+        return redirect()->back()->with([
+            'message' => 'mahasiswa belum terdaftar pada tahun yang dipilih !',
+            'alert-type' => 'info'
+        ]);
+    }
+
+    $tahun_akademik = TahunAkademik::findOrFail($request->tahun_akademik_id);
+    $data_krs = [
+        'nim' => $request->nim,
+        'tahun_akademik_id' => $request->tahun_akademik_id,
+        'nama_lengkap' => $mhs->nama_lengkap,
+        'tahun_akademik' => $tahun_akademik->tahun_akademik,
+        'semester' => $tahun_akademik->semester,
+        'prody' => $mhs->program_study->nama_prody,
+        'select_krs' => $select_krs
+    ];
+
+    return view('admin.krs.show', compact('data_krs'));
+}
+
+
+
     public function create($nim, $tahun_akademik_id)
     {
         $data_mata_kuliah = MataKuliah::get(['nama_mata_kuliah','id']);
         $tahun_akademik = TahunAkademik::find($tahun_akademik_id);
 
-        // return view('krs.create', compact('nim','tahun_akademik', 'data_mata_kuliah'));
         return view('admin.krs.create', compact('nim','tahun_akademik', 'data_mata_kuliah'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(KrsRequest $request)
     {
         Krs::create($request->validated() + ['nilai' => 0]);
@@ -83,26 +83,18 @@ class KrsController extends Controller
             'alert-type' => 'success'
         ]);
 
-        // return redirect()->route('admin.krs.index')->with([
-        //     'message' => 'berhasi di buat !',
-        //     'alert-type' => 'success'
-        // ]);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Krs $krs)
     {
         $data_mata_kuliah = MataKuliah::get(['nama_mata_kuliah','id']);
 
         return view('krs.edit', compact('krs', 'data_mata_kuliah'));
-        // return view('admin.krs.edit', compact('krs', 'data_mata_kuliah'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(KrsRequest $request, Krs $krs)
     {
         $krs->update($request->validated() + ['nilai' => 0]);
@@ -111,16 +103,9 @@ class KrsController extends Controller
             'message' => 'berhasil di ganti !',
             'alert-type' => 'info'
         ]);
-
-        // return redirect()->route('admin.krs.index')->with([
-        //     'message' => 'berhasil di ganti !',
-        //     'alert-type' => 'info'
-        // ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Krs $krs)
     {
         $krs->delete();
